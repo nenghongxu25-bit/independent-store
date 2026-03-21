ddc9f7b10f20e293a4478caa2ad1f17d40332974
nenghongxu25-bit <nenghongxu25@gmail.com>
2026-03-21 05:35:20 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { CATEGORY_DATA } from "@/constants/navigation";
-import CategoryCard from "@/components/CategoryCard";
+import { CATEGORY_DATA } from "independent-store/src/constants/navigation";
+import CategoryCard from "independent/src/components/CategoryCard";
 
 export default function HomePage() {
   return (
