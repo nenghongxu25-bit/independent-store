e979c528a3bba8294c8489059b1787940aabe773
Hong Miller <nenghongxu25@gmail.com>
2026-03-23 02:05:21 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,22 @@ export default function Title() {
   const shopname = "Shimmer Jewelry";
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'flexstart', alignItems: 'center', paddingTop: '0px', flexDirection: 'column',backgroundColor:'#fff'}}>
-      <h1 style={{ textAlign: 'center', color: 'gold', fontSize: '6rem' }}>
+    < div style={
+      { 
+        display: 'flex', 
+                 justifyContent: 'flexstart', 
+                 alignItems: 'center',
+                 paddingTop: '0px', 
+                 flexDirection: 'column',
+                 backgroundColor:'#fff'
+      }}>
+      <h1 style={
+      { 
+        textAlign: 'center', 
+        color: 'gold', 
+        fontSize: '6rem',
+        margin:'0'
+      }}>
         {shopname}
       </h1>
       <p>顶级实验室钻石定制</p>
