50442f4cf74c41f419e093aececba395e0aa5d06
Hong Miller <nenghongxu25@gmail.com>
2026-03-23 01:51:24 -0600

    Increase heading font size to 6rem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Title() {
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column' }}>
-      <h1 style={{ textAlign: 'center', color: 'gold', fontSize: '3rem' }}>
+      <h1 style={{ textAlign: 'center', color: 'gold', fontSize: '6rem' }}>
         {shopname}
       </h1>
       <p>顶级实验室钻石定制</p>
