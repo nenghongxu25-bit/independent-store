f9c469d2a14ef947073049c3edffddaf0472feed
Hong Miller <nenghongxu25@gmail.com>
2026-03-24 03:11:46 -0600

    Update EngagementMenu.tsx

--- src/components/Navbar/EngagementMenu.tsx
+++ src/components/Navbar/EngagementMenu.tsx
@@ -1,5 +1,4 @@
-// src/components/Navbar/EngagementMenu.tsx
-import { megaMenuStyles, containerStyles, headerStyles, listStyles, cardStyles } from './MenuStyles';
+import { megaMenuStyles, containerStyles, headerStyles, subHeaderStyles, listStyles, cardStyles } from './MenuStyles';
 
 export default function EngagementMenu() {
   return (
@@ -8,15 +7,37 @@ export default function EngagementMenu() {
         <div style={{ flex: 1 }}>
           <h4 style={headerStyles}>Design Your Own</h4>
           <ul style={listStyles}>
-            <li style={{ fontWeight: 'bold' }}>Shop all Engagement Rings</li>
-            <li style={{ color: '#999', marginTop: '10px' }}>Start with a Setting</li>
+            <li style={{ fontWeight: '700', color: '#000' }}>Shop all Engagement Rings</li>
+            <span style={subHeaderStyles}>Start with a Setting</span>
             <li>Engagement Ring Settings</li>
+            <li>Matching Wedding Sets</li>
+            <span style={subHeaderStyles}>Start with a Stone</span>
+            <li>Natural Diamonds</li>
+            <li>Lab-Grown Diamonds</li>
+            <li>Color Lab-Grown Diamonds</li>
+            <li>Color Gemstones</li>
+          </ul>
+        </div>
+        <div style={{ flex: 1 }}>
+          <h4 style={headerStyles}>Featured Styles</h4>
+          <ul style={listStyles}>
+            <li>Halo</li><li>Vintage</li><li>Solitaire</li><li>Classic</li>
+            <li>Perfectly Preset</li><li>Promise Rings</li><li>New Arrivals</li>
+          </ul>
+        </div>
+        <div style={{ flex: 1 }}>
+          <h4 style={headerStyles}>Metal Types</h4>
+          <ul style={listStyles}>
+            <li>White Gold</li><li>Yellow Gold</li><li>Platinum</li><li>Rose Gold</li>
           </ul>
         </div>
         <div style={cardStyles}>
-          <h4 style={{ fontFamily: 'serif' }}>Helpful Topics</h4>
+          <h4 style={headerStyles}>Helpful Topics & Tools</h4>
           <ul style={listStyles}>
             <li>How to Pick a Ring</li>
+            <li>Diamond Buying Guide</li>
+            <li>Ring Style Quiz</li>
+            <li>Real Proposals</li>
           </ul>
         </div>
       </div>
