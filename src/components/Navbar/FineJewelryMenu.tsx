b0040475b0216bedbd73d74078130611dc43b0eb
Hong Miller <nenghongxu25@gmail.com>
2026-03-24 03:08:10 -0600

    Update FineJewelryMenu.tsx

--- src/components/Navbar/FineJewelryMenu.tsx
+++ src/components/Navbar/FineJewelryMenu.tsx
@@ -7,19 +7,28 @@ export default function FineJewelryMenu() {
         <div style={{ flex: 1 }}>
           <h4 style={headerStyles}>Shop by Category</h4>
           <ul style={listStyles}>
-            <li>Necklaces</li><li>Rings</li><li>Earrings</li><li>Bracelets</li><li>Men's</li>
+            <li>Necklaces</li>
+            <li>Rings</li>
+            <li>Earrings</li>
+            <li>Bracelets</li>
+            {/* 使用 &apos; 代替单引号 */}
+            <li>Men&apos;s</li>
           </ul>
         </div>
         <div style={{ flex: 1 }}>
           <h4 style={headerStyles}>Popular Styles</h4>
           <ul style={listStyles}>
-            <li>Gold Essentials</li><li>Hoop Earrings</li><li>Tennis Bracelets</li>
+            <li>Gold Essentials</li>
+            <li>Hoop Earrings</li>
+            <li>Tennis Bracelets</li>
           </ul>
         </div>
         <div style={{ flex: 1 }}>
           <h4 style={headerStyles}>Gifts</h4>
           <ul style={listStyles}>
-            <li>Anniversary Rings</li><li>Family Jewelry</li><li>Gift Cards</li>
+            <li>Anniversary Rings</li>
+            <li>Family Jewelry</li>
+            <li>Gift Cards</li>
           </ul>
         </div>
       </div>
