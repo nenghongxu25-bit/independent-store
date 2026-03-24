3ded568b6c7327c85fcd2a2e8cd2ea2e21f69710
Hong Miller <nenghongxu25@gmail.com>
2026-03-24 03:00:01 -0600

    Create DiamondsMenu.tsx

---
+++ src/components/Navbar/DiamondsMenu.tsx
@@ -0,0 +1,34 @@
+import { megaMenuStyles, containerStyles, headerStyles, listStyles, cardStyles } from './MenuStyles';
+
+export default function DiamondsMenu() {
+  return (
+    <div style={megaMenuStyles}>
+      <div style={containerStyles}>
+        <div style={{ flex: 1 }}>
+          <h4 style={headerStyles}>Shop All Diamonds</h4>
+          <ul style={listStyles}>
+            <li style={{ fontWeight: 'bold' }}>Shop All</li>
+            <li>Natural Diamonds</li>
+            <li>Lab-Grown Diamonds</li>
+            <li>Color Lab-Grown Diamonds</li>
+          </ul>
+        </div>
+        <div style={{ flex: 1 }}>
+          <h4 style={headerStyles}>Specialty Diamonds</h4>
+          <ul style={listStyles}>
+            <li>Shane Classic Natural</li>
+            <li>Stargazer Lab-Grown</li>
+            <li>Padma Lab-Grown</li>
+          </ul>
+        </div>
+        <div style={cardStyles}>
+          <h4 style={{ fontSize: '1rem', marginBottom: '15px' }}>Diamond Education</h4>
+          <ul style={listStyles}>
+            <li style={{ textDecoration: 'underline' }}>Diamond Buying Guide</li>
+            <li style={{ textDecoration: 'underline' }}>About Lab-Grown</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
