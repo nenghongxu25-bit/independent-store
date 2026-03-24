65137d19eebca872a7e4b3578c533dd352b0b053
Hong Miller <nenghongxu25@gmail.com>
2026-03-24 03:01:06 -0600

    Create FineJewelryMenu.tsx

---
+++ src/components/Navbar/FineJewelryMenu.tsx
@@ -0,0 +1,28 @@
+import { megaMenuStyles, containerStyles, headerStyles, listStyles } from './MenuStyles';
+
+export default function FineJewelryMenu() {
+  return (
+    <div style={megaMenuStyles}>
+      <div style={containerStyles}>
+        <div style={{ flex: 1 }}>
+          <h4 style={headerStyles}>Shop by Category</h4>
+          <ul style={listStyles}>
+            <li>Necklaces</li><li>Rings</li><li>Earrings</li><li>Bracelets</li><li>Men's</li>
+          </ul>
+        </div>
+        <div style={{ flex: 1 }}>
+          <h4 style={headerStyles}>Popular Styles</h4>
+          <ul style={listStyles}>
+            <li>Gold Essentials</li><li>Hoop Earrings</li><li>Tennis Bracelets</li>
+          </ul>
+        </div>
+        <div style={{ flex: 1 }}>
+          <h4 style={headerStyles}>Gifts</h4>
+          <ul style={listStyles}>
+            <li>Anniversary Rings</li><li>Family Jewelry</li><li>Gift Cards</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
