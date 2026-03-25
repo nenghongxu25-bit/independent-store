eeb29d34e694880a93e9efce4a750e22a0a17c3d
Hong Miller <nenghongxu25@gmail.com>
2026-03-25 02:13:00 -0500

    Update WeddingMenu.tsx

--- src/components/Navbar/menus/WeddingMenu.tsx
+++ src/components/Navbar/menus/WeddingMenu.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { containerStyles, headerStyles, listStyles, cardStyles } from './MenuStyles';
+import { containerStyles, headerStyles, listStyles, cardStyles } from '../MenuStyles';
 
 export default function WeddingMenu() {
   return (
