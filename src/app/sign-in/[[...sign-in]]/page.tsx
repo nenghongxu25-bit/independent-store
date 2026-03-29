e3d63b2e3425011b7e0ff05cb94d5af4a8a956f0
nenghongxu25-bit <nenghongxu25@gmail.com>
2026-03-29 06:43:27 -0600

    feat: 在登录页开启注册入口

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,15 +1,27 @@
 import { SignIn } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function SignInPage() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-black">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-black">
+      {/* 登录框 */}
       <SignIn 
         routing="path" 
         path="/sign-in" 
-        // 🚀 就是这一行！加上它，登录框底部就会出现 "Don't have an account? Sign up"
-        signUpUrl="/sign-up" 
+        signUpUrl="/sign-up"
         afterSignInUrl="/account"
       />
+      
+      {/* 强制显示的注册入口 */}
+      <div className="mt-6 text-center">
+        <p className="text-gray-400 mb-2">没有账号？</p>
+        <Link 
+          href="/sign-up" 
+          className="text-white border border-white px-6 py-2 rounded-full hover:bg-white hover:text-black transition"
+        >
+          立即注册 Shimmer 会员
+        </Link>
+      </div>
     </div>
   );
 }
